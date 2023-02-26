ca27e3dfa8e2fbc45d8ce8d9da8f656f7eb08346
Samet Arslan <92922917+alpagoars@users.noreply.github.com>
2023-02-26 21:37:27 +0300

    Add files via upload

---
+++ ucgencizme/ucgencizme/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ucgencizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" kenar uzunluğunu giriniz: ");
+            int Uzunluk = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= Uzunluk; i++)
+            {
+                for (int j = 1; j <= Uzunluk - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
