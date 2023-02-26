7f1c6f62b1d8d984db0d5170400f3c3d96983899
Samet Arslan <92922917+alpagoars@users.noreply.github.com>
2023-02-26 22:13:49 +0300

    Add files via upload

---
+++ dairecizme/dairecizme/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dairecizme
+{
+    internal class Program
+    {
+       
+
+
+             void daireciz(int s)
+            {
+        
+            Console.WriteLine("dairenin yarıçapını giriniz:");
+            int s = int.Parse(Console.ReadLine());
+
+
+            daireciz(s);
+
+            for (double y = -s; y <= s; y++)
+                {
+                    for (double x = -s; x <= s; x += 0.5)
+                    {
+                        if ((x * x) + (y * y) >=    s * s)
+                            Console.Write("*");
+                        else
+                            Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+
