2bd3078991cf0ad8f10e1a44ae6cb12bce73dc11
Samet Arslan <92922917+alpagoars@users.noreply.github.com>
2023-03-01 20:08:36 +0300

    Add files via upload

---
+++ ters_karakter/ters_karakter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ters_karakter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("metninizi giriniz: ");
+            string metin = Console.ReadLine();
+            string tersmetin = "";
+
+            for (int i = metin.Length - 1; i >= 0; i--)
+            {
+                tersmetin += metin[i];
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Ters metin: " + tersmetin);
+            Console.ReadLine();
+        }
+    }
+}
