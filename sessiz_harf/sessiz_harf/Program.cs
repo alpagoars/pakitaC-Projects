334277af62d25f4cd7d95f6d5fcaa13835938623
Samet Arslan <92922917+alpagoars@users.noreply.github.com>
2023-03-01 20:12:13 +0300

    Add files via upload

---
+++ sessiz_harf/sessiz_harf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sessiz_harf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin girin: ");
+            string metin = Console.ReadLine();
+
+            string[] kelime = metin.Split(' ');
+            string consonants = "bcdfghjklmnpqrstvwxyz";
+
+            foreach (string word in kelime)
+            {
+                bool hasConsecutiveConsonants = false;
+
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    if (consonants.Contains(kelime[i]) && consonants.Contains(kelime[i + 1]))
+                    {
+                        hasConsecutiveConsonants = true;
+                        break;
+                    }
+                }
+
+                Console.Write(hasConsecutiveConsonants + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
